75cc1852301489ff938cc751f6553fd3188376e6
Amritesh Kuraria <59308003+Amritesh15@users.noreply.github.com>
2025-11-19 23:26:10 -0500

    login signup frontend page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Auth from './pages/Auth/Auth'
-import { Routes, Route, Router } from 'react-router-dom'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/dashboard/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <Router>
-    <Routes>
-      <Route path="/signup" element={<Auth type="signup"/>} />
-      <Route path="/login" element={<Auth type="login"/>} />
-    </Routes>
-   </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Auth type="signup"/>} />
+        <Route path="/login" element={<Auth type="login"/>} />
+      </Routes>
+    </BrowserRouter>
   )
 } 
 
